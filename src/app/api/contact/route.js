7d5d99a579e36bc7322a4cb41e1e8f64f203f4d8
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-07-27 00:35:22 +0600

    backend bug fixed

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -16,8 +16,6 @@ export async function POST(request) {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS,   
       },
-      logger: true, // ডিবাগিং চালু করুন
-      debug: true   // ডিবাগিং চালু করুন
     });
 
     // Set up email data
